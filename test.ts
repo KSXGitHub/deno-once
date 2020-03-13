bafce80711c6cd6ec4993ecbdd33de960b658b21
khai96_ <hvksmr1996@gmail.com>
2020-03-13 17:01:25 +0700

    Remove TypeScript file and correct Triple-Slash Reference

--- test.ts
+++ test.ts
@@ -1,5 +1,5 @@
 import { assertStrictEq } from 'https://deno.land/std@v0.35.0/testing/asserts.ts'
-import DEFAULT, { once } from './index.ts'
+import DEFAULT, { once } from './index.js'
 
 Deno.test('default export', () => {
   assertStrictEq(once, DEFAULT)
