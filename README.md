7ec7cc3d60d8f295fd5d6815df4e95ff028501a4
khai96_ <hvksmr1996@gmail.com>
2020-03-13 13:58:45 +0700

    Fix URL again

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Create a function that calls and cache a function once
 ## Usage
 
 ```typescript
-import once from 'https://ksxgithub.github.io/once/index.js'
+import once from 'https://ksxgithub.github.io/deno-once/index.js'
 const ran = once(Math.random)
 console.log(ran() === ran()) // => true
 ```
--- index.js
+++ index.js
@@ -5,7 +5,7 @@
  * @returns {() => Return} Function that returns result of first-time execution of `fn`
  *
  * @example
- *   import once from 'https://ksxgithub.github.io/once/index.js'
+ *   import once from 'https://ksxgithub.github.io/deno-once/index.js'
  *   const ran = once(Math.random)
  *   console.log(ran() === ran()) // => true
  *
