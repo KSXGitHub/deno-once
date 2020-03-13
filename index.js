82f8eb986d303a5711c9604964472e90e0fa3aa4
khai96_ <hvksmr1996@gmail.com>
2020-03-13 13:55:59 +0700

    Convert to .js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 /**
  * Create a function that calls and cache a function once
- * @param fn Function to be invoked once
- * @returns Function that returns result of first-time execution of `fn`
+ * @template Return
+ * @param {() => Return} fn Function to be invoked once
+ * @returns {() => Return} Function that returns result of first-time execution of `fn`
  */
-export function once<Return> (fn: () => Return): () => Return {
+export function once (fn) {
   let main = () => {
     const value = fn()
     main = () => value
